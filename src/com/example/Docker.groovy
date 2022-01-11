f10a5aa6026e704591e93a8817eeca3425a7006d
akshayca <akshay.ca.07@gmail.com>
2022-01-11 22:37:28 +0530

    Initial commit

---
+++ src/com/example/Docker.groovy
@@ -0,0 +1,27 @@
+#!/usr/bin/env groovy
+package com.example
+
+class Docker implements Serializable {
+
+    def script
+
+    Docker(script) {
+        this.script = script
+    }
+
+    def buildDockerImage(String imageName) {
+        script.echo "building the docker image..."
+        script.bat "docker build -t %imageName% ."
+    }
+
+    def dockerLogin() {
+        script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+            script.bat "docker login -u %script.USER% -p %script.PASS%"
+        }
+    }
+
+    def dockerPush(String imageName) {
+        script.bat "docker push %imageName%"
+    }
+
+}
