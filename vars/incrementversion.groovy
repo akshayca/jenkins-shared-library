61801451b760842ac0882484b9a0d6fbbf7607c3
akshayca <akshay.ca.07@gmail.com>
2022-01-12 23:28:48 +0530

    Updated the version increment code

--- vars/incrementversion.groovy
+++ vars/incrementversion.groovy
@@ -3,6 +3,6 @@
 def call() {
     echo 'incrementing app version...'
     bat 'mvn build-helper:parse-version versions:set \
-                        -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} \
+                        -DnewVersion=${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.nextIncrementalVersion} \
                         versions:commit'
 }
